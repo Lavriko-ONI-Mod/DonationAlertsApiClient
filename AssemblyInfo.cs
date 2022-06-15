585afe5f5a14638fa22fe4f650e0763e832ebf3b
Daniel Ovcharenko <farewellfire21@gmail.com>
2022-06-15 18:46:37 +0100

    Added assembly info.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿[assembly: System.Reflection.AssemblyFileVersionAttribute("0.7.2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.7.2")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.7.2")]
